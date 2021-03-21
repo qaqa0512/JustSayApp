25d349ffbf377d1c6225e500a7bd65868b43dda5
tintran259 <nguyenquocanh0512@gmail.com>
2021-03-21 20:11:58 +0700

    Adding more functions for HomeScreen

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {View, Text, StyleSheet, Image, Button, TouchableOpacity, Alert} from 'react-native';
 
 import Icons from 'react-native-vector-icons/FontAwesome'
 import Guide from 'react-native-vector-icons/Feather'
+import Other from 'react-native-vector-icons/Foundation'
+
+import {Picker} from '@react-native-picker/picker';
 
 import images from '../Constants/image'
 
 export default function HomeScreen() {
+
+    const [selectedValue, setSelectedValue] = useState("Việt");
 
     const clear = () => {
         Alert.alert("Your conservation is cleared!")
@@ -17,6 +22,11 @@ export default function HomeScreen() {
         Alert.alert("Hello friend!")
     };
 
+    const record = () => {
+        Alert.alert("I'm hearing")
+    };
+    
+
     return(
         <View style={styles.sectionContainer}>
             {/* Header */}
@@ -24,7 +34,6 @@ export default function HomeScreen() {
                 <View style={styles.headerMain}>
                     <View style={styles.headerTxtBox}>
                         <Image source={images.Logo} style={styles.logoHomeImg}/>
-                        {/* <Text style={styles.headerText}>Just Say</Text> */}
                     </View>
                     <TouchableOpacity onPress={info} style={styles.IconClick}>
                         <Guide name="info" size={26} style={styles.infoIcon}/>
@@ -37,9 +46,58 @@ export default function HomeScreen() {
 
             {/* Body */}
             <View style={styles.bodyContainer}>
-                <Text>Body Container</Text>
-            </View>
+                <View style={styles.recordContainer} >
+                    <View>
+                        <TouchableOpacity onPress={record} style={styles.recordIcon}>
+                            <Other name="microphone" size={35} color="#fff"/>
+                        </TouchableOpacity>
+                        {/* <Text style={styles.txtTranslate}>Việt</Text> */}
+                    </View>
+                    <View style={{marginBottom:30}} >
+                        <Picker
+                            selectedValue={selectedValue}
+                            style={{ height: 50, width: 110,}}
+                            onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+                        >
+                            <Picker.Item label="Việt" value="vie" />
+                            <Picker.Item label="Anh" value="en" />
+                            <Picker.Item label="Hàn" value="korea" />
+                            <Picker.Item label="Pháp" value="france" />
+                        </Picker>
+                    </View>
 
+                    <View>
+                        <TouchableOpacity onPress={record} style={styles.reverseIcon}>
+                            <Other name="microphone" size={35} color="#fff"/>
+                        </TouchableOpacity>
+                    </View>
+                    <View>
+                        <Picker
+                            style={{ height: 50, width: 110,}}
+                        >
+                            <Picker.Item label="Anh" value="en" />
+                            <Picker.Item label="Việt" value="vie" />
+                            <Picker.Item label="Hàn" value="korea" />
+                            <Picker.Item label="Pháp" value="france" />
+                        </Picker>
+                    </View>
+                </View>
+                <View style={styles.conservationContainer}>
+                    {/*  */}
+                    <View style={styles.displayContainer}>
+                        {/*  */}
+                        <View style={styles.titleContainer}>
+                            <Text>Việt</Text>
+                            <Text style={{marginHorizontal:5}}>-</Text>
+                            <Text>Anh</Text>
+                        </View>
+                        {/*  */}
+                        <View style={styles.translateContainer}>
+                            
+                        </View>
+                    </View>
+                </View>
+            </View>
         </View>
     );
 };
@@ -103,12 +161,62 @@ const styles = StyleSheet.create({
         elevation: 20,
     },
     // Body Container
-
     bodyContainer:
     {
         flex:1,
-        backgroundColor:'#fff',
+        flexDirection:'row',
+    },
+    // ------------------
+    recordContainer:{
         alignItems:'center',
         justifyContent:'center',
-    }
+        paddingTop:35,
+        marginHorizontal:10,
+        marginVertical:10,
+        backgroundColor:'#fff',
+        borderRadius:10,
+    },
+    recordIcon:{
+        borderWidth:1,
+        borderColor:'#F8C491',
+        paddingHorizontal:25,
+        paddingVertical:7,
+        borderRadius:10,
+        backgroundColor:'#FFDCBA',
+    },
+    txtTranslate:{
+        textAlign:'center',
+        fontSize:17,
+        marginTop:7,
+    },
+    //-------------------
+    reverseIcon:{
+        borderWidth:1,
+        borderColor:'#3DD4CB',
+        paddingHorizontal:25,
+        paddingVertical:7,
+        borderRadius:10,
+        backgroundColor:'#3DD4CB',
+    },
+
+    // ConservationContainer
+    conservationContainer:{
+        flex:1,
+        paddingHorizontal:8,
+        paddingVertical:10,
+        
+    },
+    //---------------------
+    displayContainer:{
+        backgroundColor:'#fff',
+        flex:1,
+        borderRadius:10,
+    },
+    //---------------------
+    titleContainer:{
+        flex:1,
+        flexDirection:'row',
+        justifyContent:'center',
+        marginVertical:10,
+    },
   });
